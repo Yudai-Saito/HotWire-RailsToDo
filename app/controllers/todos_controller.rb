9da7018220c9d7a0b5e50a57013958fe3ab399f6
YudaiSaito <yudai.saito.dev@gmail.com>
2024-05-10 22:33:41 +0900

    feat: todoアプリのコントローラー

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,34 @@
 class TodosController < ApplicationController
+  def index
+    session[:todos] ||= []
+    session[:completed_todos] ||= []
+    render layout: false
+  end
+
+  def create
+    session[:todos] << params[:todo]
+    redirect_to todos_path
+  end
+
+  def destroy
+    session[:todos].delete_at(params[:id].to_i)
+    redirect_to todos_path
+  end
+
+  def complete
+    task = session[:todos].delete_at(params[:id].to_i)
+    session[:completed_todos] << task
+    redirect_to todos_path
+  end
+
+  def uncomplete
+    task = session[:completed_todos].delete_at(params[:id].to_i)
+    session[:todos] << task
+    redirect_to todos_path
+  end
+
+  def destroy_completed
+    session[:completed_todos].delete_at(params[:id].to_i)
+    redirect_to todos_path
+  end
 end
